6767d3aedb9eae569d3d47010f8e1af429ec9a1c
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-04-08 00:13:46 -0400

    Set up flash, success & errors partial

--- routes/reviews.js
+++ routes/reviews.js
@@ -31,6 +31,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash('success', 'Created new review')
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
@@ -45,6 +46,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
         }
     });
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted review')
     res.redirect(`/campgrounds/${id}`);
 }))
 
